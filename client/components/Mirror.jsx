5224d8aa95beb7961cb7a90f8c1134748fd38aa9
Juan Espinoza <juanespinoza@Juans-MacBook-Air.local>
2019-09-17 15:29:10 -0700

    CodeMirror formatting string into JavaScript syntax correctly

--- client/components/Mirror.jsx
+++ client/components/Mirror.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 // import {UnControlled as CodeMirror} from 'react-codemirror2'
 import CodeMirror from 'react-codemirror';
 
+// import 'codemirror-formatting';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/lib/codemirror.js';
+
 import 'codemirror/mode/javascript/javascript.js';
 
+let beautify = require('js-beautify').js;
+
+const value = 'describe("addUser", () => { it("should return an object") }); function() { console.log("arrow syntax?")}; describe("addUser", () => { it("should return an object") }); function() { console.log("arrow syntax?")};describe("addUser", () => { it("should return an object") }); function() { console.log("arrow syntax?")};describe("addUser", () => { it("should return an object") }); function() { console.log("arrow syntax?")};describe("addUser", () => { it("should return an object") }); function() { console.log("arrow syntax?")};describe("addUser", () => { it("should return an object") }); function() { console.log("arrow syntax?")};'
+
 const mirror = (props) => {
+  const [js, setJs] = useState('');
+  // let date = new Date();
+
+  useEffect(() => {
+    let formattedJs = beautify(value, {indent_size: 2});
+    setJs(formattedJs);
+    // console.log('useEffect', Date.now());
+    console.log('printing out formatted js\n', js);
+    console.log(typeof js);
+  })
 
   return (
     <div id='mirror'>
       <CodeMirror
-        value='describe("addUser"){ it("should return an object")}'
         options={{
-          mode: {name:'javascript'},
-          theme: 'ambiance',
+          value: js,
+          mode: 'javascript',
+          theme: 'material',
+          lineNumbers: true,
+          readOnly: true,
           tabsize: 2,
-          lineNumbers: true
-        }}
-        onChange={(editor, data, value) => {
         }}
       />
     </div>
