008f178b1c5e033b3ef3d717041f6675eb4ebbb0
Juan Espinoza <juanespinoza@Juans-MacBook-Air.local>
2019-09-16 21:58:36 -0700

    migrated to react-codemirror

--- client/components/Mirror.jsx
+++ client/components/Mirror.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {UnControlled as CodeMirror} from 'react-codemirror2'
-// import 'codemirror/mode/javscript/javscript';
-function mirror() {
+// import {UnControlled as CodeMirror} from 'react-codemirror2'
+import CodeMirror from 'react-codemirror';
+
+import 'codemirror/mode/javascript/javascript.js';
+
+const mirror = (props) => {
 
   return (
     <div id='mirror'>
       <CodeMirror
-        value='(arg) => {console.log(arg)};'
+        value='describe("addUser"){ it("should return an object")}'
         options={{
           mode: {name:'javascript'},
           theme: 'ambiance',
-          lint: true,
-          line: true,
-          eletricChars: true
+          tabsize: 2,
+          lineNumbers: true
         }}
         onChange={(editor, data, value) => {
         }}
