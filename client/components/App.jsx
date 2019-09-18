649fd31d4f227d69d5dc2530625c2b8fa9667bba
Jimmy <jimmy@Jimmys-MacBook-Air.local>
2019-09-17 19:15:10 -0700

    working components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,43 @@
 import React, {useState} from 'react';
 import Right from './Right.jsx'
-import Left from './Left.jsx'
+import Func from './Func.jsx'
 import './styles.css'
 import 'tachyons'
 function App() {
-  const [State, setState] = useState(null)
-  const addFun = () =>{
+  const [functions, setFunc] = useState([{functionName:'test', tests: ['should render an object']}])
+  const [inputField, setInput] = useState('')
+  const [inputTest, setTest] = useState('')
 
+  const addFun = () =>{
+    let newFunctions = [...functions];
+    newFunctions.push({'functionName':inputField, tests:[]})
+    setFunc(newFunctions);
+    setInput('');
   }
+  
+  const inputChange = (e)=>{
+    setInput(e.target.value)
+  }
+
+  let newFuncList = functions.map((el,index) => {
+    return <Func key={el.functionName + index} index={index} functions={functions} 
+    setFunc={setFunc} name={el.functionName} tests={el.tests} id={index}
+    setTest={setTest} inputTest={inputTest}
+    />
+  })
   return(
-    <div>
-    <Right/>
-    <Left/>
+    <div style={{width:300}}>
+      {newFuncList}
+      <div>
+        <div className='dib br3 pa3 ma2 bw2 shadow-5'>
+          <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" 
+            type="text" aria-describedby="name-desc" value={inputField} onChange={inputChange}>
+          </input>
+          <button className='addTest bg-light-green ma2 grow bw2 shadow-5' onClick={addFun}>
+            add
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ client/components/Func.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+
+function Func(props) {
+  const handleDelete = ()=>{
+    console.log('deleting shit')
+  }
+  const addTest = ()=>{
+    let newFuncArr=[...props.functions]
+    newFuncArr[props.index]["tests"].push(props.inputTest)
+    props.setFunc(newFuncArr);
+    props.setTest('')
+  }
+  let testArr = []
+  props.tests.forEach((test, i)=>{
+    testArr.push(<p key={i}>{test}</p>)
+  })
+
+  return(
+   <div className ='Func dib br3 pa3 ma2 bw2 shadow-5'>
+      <h3>
+        {props.name}
+      </h3>
+      {testArr} 
+        
+      <input id="name" className="input-reset ba b--black-20 pa2 mb2 db w-100" 
+      type="text" aria-describedby="name-desc" onChange={(e)=>{  
+       props.setTest(e.target.value);
+      }}>
+      </input>
+      
+      <button className='addTest bg-light-green ma2 grow bw2 shadow-5' onClick={addTest}>
+        add test
+      </button>
+      <button className='deleteTest bg-red ma2 grow bw2 shadow-5' onClick={handleDelete}>
+        delete test
+      </button>
+    </div>
+  )
+}
+
+export default Func
