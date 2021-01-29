3eee211c7a88901ea75a7be40424a9e6a824ff98
jespinoza17 <jaespin300@yahoo.com>
2021-01-29 14:33:31 -0800

    updated server and bundle js file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser')
+const fs = require('fs');
 
 
 app.use(express.static(path.resolve(__dirname,'../client')));
 
+app.use(bodyParser.json())
 
 app.get('/', (req,res,next) => {
   res.sendFile('../client/index.html')
+})
+
+app.post('/generate', (req,res,next) => {
+  console.log(req.body)
+  fs.writeFile('quickjest.test.js', req.body.formattedString, (err)=>{
+    if (err) res.status.send(err);
+    console.log('the file has been saved')
+    res.download(path.resolve(__dirname,'../quickjest.test.js'));
+  })
 })
 
 app.listen(3000, () => {
